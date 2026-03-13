553d95bba02aab0285a31669e1ff2c942155e42b
mor1mlkawi-tech <mor1mlkawi@gmail.com>
2026-03-13 19:45:17 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+function showLungs() {
+    document.getElementById("result").innerHTML =
+    "Smoking damages the lungs and can cause lung cancer.";
+}
 
+function showHeart() {
+    document.getElementById("result").innerHTML =
+    "Smoking increases the risk of heart disease.";
+}
+
+function showBrain() {
+    document.getElementById("result").innerHTML =
+    "Smoking affects blood flow to the brain and increases stroke risk.";
+}
